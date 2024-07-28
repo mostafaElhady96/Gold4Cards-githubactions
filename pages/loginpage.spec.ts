68ab8b1fd7d9d2a973bf2cfa34c19cef9a3d9c34
Mostafa Elhady <mostafaElhady96@github.com>
2024-07-28 12:22:16 +0300

    update

--- pages/loginpage.spec.ts
+++ pages/loginpage.spec.ts
@@ -4,19 +4,24 @@ export class Loginpage {
   readonly page: Page;
   readonly emailfield:Locator;
   readonly passwordfield :Locator;
-  readonly submitbtninloginpage
+  readonly submitbtninloginpage:Locator;
+  readonly notificationicon:Locator;
+
   constructor(page: Page) {
     this.page = page;
     this.emailfield = page.getByPlaceholder('Type your email here');
     this.passwordfield = page.getByPlaceholder('Type your password here');
     this.submitbtninloginpage=page.getByLabel('submit');
+    this.notificationicon=page.locator('#Path_1127');
   
   }
 
-  async loginwithvalidcredentials() {
+  async loginwithvalidcredentials(validemail,validpassword) {
     await this.page.goto('https://gold4cards.com/auth/login');
-    await this.emailfield.fill('mostafaelhadytester@gmail.com');
-    await this.passwordfield.fill('Mostafa11223355@');
+   // await this.emailfield.fill('mostafaelhadytester@gmail.com');
+    await this.emailfield.fill(validemail);
+   // await this.passwordfield.fill('Mostafa11223355@');
+    await this.passwordfield.fill(validpassword);
     await this.submitbtninloginpage.click();
    
   }
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -3,9 +3,12 @@ import { homepage } from '../pages/homepage.spec';
 import { Loginpage } from '../pages/loginpage.spec';
 
 test('login with my email', async ({ page }) => {
+  
     
     const loginpage= new Loginpage(page);
-    await loginpage.loginwithvalidcredentials();
+    await loginpage.loginwithvalidcredentials('mostafaelhadytester@gmail.com','Mostafa11223355@');
+  
+    await expect(loginpage.notificationicon).toBeVisible();
     page.close();
     
   });
